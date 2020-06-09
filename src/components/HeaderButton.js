8381dbd694894eda9b104b659fad2678c977b6a3
Sonia <soniawadji@gmail.com>
2020-06-09 17:18:44 +0530

    Created basic styled button for header

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -5,7 +5,13 @@ const HeaderButton = ({ title }) => {
     return (
         <TouchableOpacity>
             <View style={styles.buttonBackground}>
-                <Text style={styles.buttonText}>{title}</Text>
+                <Image
+                    style={styles.buttonImage}
+                    source={require("../../assets/icon.png")}>
+                </Image>
+                <Text
+                    style={styles.buttonText}>{title}
+                </Text>
             </View>
         </TouchableOpacity>
     )
@@ -14,9 +20,20 @@ const HeaderButton = ({ title }) => {
 const styles = StyleSheet.create({
     buttonBackground: {
         backgroundColor: 'black',
+        flexDirection: "row",
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        alignItems: 'center'
+    },
+    buttonImage: {
+        width: 30,
+        height: 30,
+        marginEnd: 10
     },
     buttonText: {
-        color: 'white'
+        color: 'white',
+        fontSize: 18
     }
 });
 
