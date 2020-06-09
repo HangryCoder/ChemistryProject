79763418fe02257d05969dff09ac4694e514d304
Sonia <soniawadji@gmail.com>
2020-06-09 16:13:42 +0530

    Added React Navigation to open HomeScreen

---
+++ screen/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    Image,
+    Button,
+    StyleSheet
+} from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Chemistry</Text>
+            <Text>3 topics</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
