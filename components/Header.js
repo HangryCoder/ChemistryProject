188b12e40a759f0185b5719d566fb017a2dafb60
Sonia <soniawadji@gmail.com>
2020-06-09 16:39:36 +0530

    Created a separate component for Header

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Image, View, Text, StyleSheet } from 'react-native';
+import { red, white } from 'color-name';
+
+const Header = () => {
+    return (<View style={styles.headerContainer}>
+        <Image source={require("../assets/icon.png")}></Image>
+        <Text style={styles.headerText}>Chemistry</Text>
+        <Text style={styles.headerText}>3 topics</Text>
+        <Button
+            title="Start pratice"
+        />
+    </View >)
+};
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        flex: 1,
+        backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerText: {
+        color: 'white'
+    }
+});
+
+export default Header;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    Image,
+    Button,
+    StyleSheet
+} from 'react-native';
+import Header from '../components/Header'
+
+const HomeScreen = () => {
+    return (
+        <Header></Header>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
