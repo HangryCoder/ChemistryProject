c59cd5bae4d98424d1707e12de1fe23333975e7b
Sonia <soniawadji@gmail.com>
2020-06-09 23:22:00 +0530

    Added Bottom Sheet dependency

---
+++ src/components/CustomBottomSheet.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import BottomSheet from 'reanimated-bottom-sheet'
+
+const CustomBottomSheet = () => {
+    return (
+        <View>
+            <BottomSheet
+            />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default CustomBottomSheet;
