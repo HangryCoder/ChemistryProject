83ae099dc9456cf186df86af98d6c9f52c6bcbea
Sonia <soniawadji@gmail.com>
2020-06-10 20:25:10 +0530

    Hid the toolbar

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, HeaderBackButton } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen'
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false
+          }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
