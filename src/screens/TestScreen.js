e26692d638194054466f1635577264bacca353ac
Sonia <soniawadji@gmail.com>
2020-06-23 17:14:52 +0530

    Rendering subject and subsections data. Still needs more changes

--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -4,6 +4,7 @@ import BottomSheet from 'reanimated-bottom-sheet';
 import SubjectList from '../components/subject/SubjectList';
 import StartPracticeBottomSheetContent from '../components/bottomsheets/startPractice/StartPracticeBottomSheetContent';
 import CustomBottomSheetHeader from '../components/bottomsheets/CustomBottomSheetHeader';
+import SubjectBottomSheetContent from '../components/bottomsheets/subSection/SubjectBottomSheetContent';
 
 const snapPoints = [
     450,
@@ -15,7 +16,24 @@ class TestScreen extends React.Component {
     constructor(props) {
         super(props);
         this.bottomSheetRef = React.createRef();
+        this.state = {
+            subject: {},
+            subSections: []
+        };
     }
+
+    startPractice() {
+        this.setState({ subject: null, subSections: null });
+        this.openBottomSheet()
+    }
+
+    fetchSubjectData(subjectData) {
+        this.setState({
+            subject: subjectData,
+            subSections: subjectData.sub_sections
+        });
+        this.openBottomSheet()
+    };
 
     openBottomSheet() {
         this.bottomSheetRef.current.snapTo(0);
@@ -25,11 +43,34 @@ class TestScreen extends React.Component {
         this.bottomSheetRef.current.snapTo(1);
     }
 
+    renderBottomSheetHeader() {
+        const { subject, subSections } = this.state;
+
+        const title = subject ? subject.name : "Chemistry";
+        const topicCount = subSections ? subSections.length : "7";
+
+        return <CustomBottomSheetHeader
+            title={title}
+            subTitle={topicCount}
+            onPress={() => this.bottomSheetRef.current.snapTo(0)} />;
+    }
+
+    renderBottomSheetContent() {
+        const { subject } = this.state;
+
+        return (subject ? <SubjectBottomSheetContent
+            subSections={subject.sub_sections}
+            selectedSubSectionsCountCallback={(count) => getCountOfSelectedSubSections(count)}
+        /> : <StartPracticeBottomSheetContent />);
+    }
+
     render() {
         return (
             <View style={styles.mainContainer}>
                 <TouchableWithoutFeedback onPress={() => this.closeBottomSheet()}>
-                    <SubjectList startPracticeCallback={() => this.openBottomSheet()} />
+                    <SubjectList startPracticeCallback={() => this.startPractice()}
+                        fetchSubjectData={(subject) => this.fetchSubjectData(subject)}
+                    />
                 </TouchableWithoutFeedback>
                 <BottomSheet
                     ref={this.bottomSheetRef}
@@ -37,11 +78,8 @@ class TestScreen extends React.Component {
                     enabledHeaderGestureInteraction={true}
                     enabledContentGestureInteraction={false}
                     snapPoints={snapPoints}
-                    renderHeader={() => <CustomBottomSheetHeader
-                        title="Chemistry"
-                        subTitle="7"
-                        onPress={() => this.bottomSheetRef.current.snapTo(0)} />}
-                    renderContent={() => <StartPracticeBottomSheetContent />}
+                    renderHeader={() => this.renderBottomSheetHeader()}
+                    renderContent={() => this.renderBottomSheetContent()}
                 />
             </View>
         );
