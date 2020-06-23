b3a8c459f6441d27c4d80f32865f2b89cdba61b7
Sonia <soniawadji@gmail.com>
2020-06-23 16:15:24 +0530

    Closing bottom sheet on tap of outside

--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import BottomSheet from 'reanimated-bottom-sheet';
 import SubjectList from '../components/subject/SubjectList';
 import StartPracticeBottomSheetContent from '../components/bottomsheets/CustomBottomSheetHeader';
@@ -21,22 +21,30 @@ class TestScreen extends React.Component {
         this.bottomSheetRef.current.snapTo(0);
     }
 
+    closeBottomSheet() {
+        this.bottomSheetRef.current.snapTo(1);
+    }
+
     render() {
-        return (<View style={styles.mainContainer}>
-            <SubjectList startPracticeCallback={() => this.openBottomSheet()} />
-            <BottomSheet
-                ref={this.bottomSheetRef}
-                initialSnap={1}
-                enabledHeaderGestureInteraction={true}
-                enabledContentGestureInteraction={false}
-                snapPoints={snapPoints}
-                renderHeader={() => <CustomBottomSheetHeader
-                    title="Chemistry"
-                    subTitle="7"
-                    onPress={() => this.bottomSheetRef.current.snapTo(0)} />}
-                renderContent={() => <StartPracticeBottomSheetContent />}
-            />
-        </View>);
+        return (
+            <TouchableWithoutFeedback onPress={() => this.closeBottomSheet()}>
+                <View style={styles.mainContainer}>
+                    <SubjectList startPracticeCallback={() => this.openBottomSheet()} />
+                    <BottomSheet
+                        ref={this.bottomSheetRef}
+                        initialSnap={1}
+                        enabledHeaderGestureInteraction={true}
+                        enabledContentGestureInteraction={false}
+                        snapPoints={snapPoints}
+                        renderHeader={() => <CustomBottomSheetHeader
+                            title="Chemistry"
+                            subTitle="7"
+                            onPress={() => this.bottomSheetRef.current.snapTo(0)} />}
+                        renderContent={() => <StartPracticeBottomSheetContent />}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
+        );
     }
 }
 
