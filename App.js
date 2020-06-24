2bbaeff4c08d148f75ee7fc56bcd533a8551d46b
Sonia <soniawadji@gmail.com>
2020-06-24 17:11:59 +0530

    Installed react-redux and started with implementation

--- App.js
+++ App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, HeaderBackButton } from '@react-navigation/stack';
 import TestScreen from './src/screens/TestScreen';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={TestScreen}
-          options={{
-            headerShown: false
-          }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={createStore(reducers)}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={TestScreen}
+            options={{
+              headerShown: false
+            }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
