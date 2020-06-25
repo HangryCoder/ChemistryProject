10e609aedcbdc22b61b2aa68be05ff979f9034eb
Sonia <soniawadji@gmail.com>
2020-06-25 16:47:35 +0530

    Added constant for Selected Sub Sections reducer and started with implementation

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const DECREMENT_COUNTER = 'decrement_counter';
 export const STEP = 5;
 
 export const SELECTED_SUBJECT_ID = 'selected_subject_id';
+export const SELECTED_SUB_SECTIONS = 'selected_sub_sections';
--- src/reducers/SelectedSubSectionsReducer.js
+++ src/reducers/SelectedSubSectionsReducer.js
@@ -1,3 +1,10 @@
-export default () => {
-    return null;
+import { SELECTED_SUB_SECTIONS } from '../constants';
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case SELECTED_SUB_SECTIONS:
+            return state;
+        default:
+            return state;
+    }
 };
